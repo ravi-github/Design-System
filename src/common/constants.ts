f8132d9b96f367b15bb22b46ceded9da576a6132
raki <jobs4raki87@gmail.com>
2021-01-06 04:53:41 +0530

    Initialize project using Create React App

---
+++ src/common/constants.ts
@@ -0,0 +1,3 @@
+
+
+export const exampleFolder = "Example";
---
+++ src/stories/CarouselTrial/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const MainContainer = styled("div")`
+    padding: 50px;
+    border: 1px solid red;
+`;
+
+export const Img = styled("img")<any>`
+    width: calc(100% / 3);
+    height: 90%;
+`;
