c395a6f0fa97d02af93de6838ca6b68cac7280c8
Ravikiran Tadepalli <travikiran@tekion.com>
2022-03-06 18:19:09 +0530

    Create JS skeleton

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+        TRIAL 2
+    </div>
+  );
+}
+
+export default App;
